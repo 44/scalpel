08ecafacf469ce2c2fc1f9715b3853b89228fe97
Andrey Ustinov <austinov@gmail.com>
2023-11-21 16:04:54 -0800

    chore: add log formatter

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"os"
 	"github.com/spf13/cobra"
+	log "github.com/sirupsen/logrus"
 	"fmt"
 )
 
@@ -11,8 +12,41 @@ var rootCmd = &cobra.Command{
 	Short: "toolset for dealing with logs",
 	PersistentPreRun: func(cmd *cobra.Command, arg []string) {
 		verbose, _ := cmd.Flags().GetCount("verbose")
-		fmt.Println("Configure verbosity here", verbose)
+		switch verbose {
+		case 0:
+			log.SetLevel(log.WarnLevel)
+		case 1:
+			log.SetLevel(log.InfoLevel)
+		case 2:
+			log.SetLevel(log.DebugLevel)
+		case 3:
+			log.SetLevel(log.TraceLevel)
+		}
+		log.SetFormatter(&logFormatter{ name: "scalpel" })
 	},
+}
+
+type logFormatter struct {
+	name string
+}
+
+func levelToString(l log.Level) string {
+	switch l {
+	case log.PanicLevel, log.FatalLevel, log.ErrorLevel:
+		return "ERR"
+	case log.WarnLevel:
+		return "WRN"
+	case log.InfoLevel:
+		return "INF"
+	case log.DebugLevel, log.TraceLevel:
+		return "DBG"
+	}
+	return "TRC"
+}
+
+func (self *logFormatter) Format(e *log.Entry) ([]byte, error) {
+	msg := fmt.Sprintf("%3s [%-10s] %s\n", levelToString(e.Level), self.name, e.Message)
+	return []byte(msg), nil
 }
 
 func Execute() {
